bb4d89f20444d3c23665c7f2deaf92496f08ad41
Chuka E <chuka.ue@gmail.com>
2021-07-23 08:51:22 +0100

    Hi, Branch!

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Fast<a href="https://nextjs.org">Feedback!</a>
         </h1>
 
         <p className={styles.description}>
